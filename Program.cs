244b7ed06a5a174c078d241640fdfa9b22cb88c3
«Pavel <kskles@yandex.ru>
2023-04-22 20:29:15 +0700

    Решение задачи 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа
+//и выдаёт, какое число большее, а какое меньшее.
+int a;
+int b;
+
+Console.WriteLine("Введите первое число");
+
+ a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+ b = Convert.ToInt32(Console.ReadLine());
+
+ if(a>b)
+ {
+    
+    object? а = null;
+    Console.WriteLine("{0} наибольшое число", а);
+ }
+ else
+ {
+   Console.WriteLine("{0} наибольшое число", b); 
+ }
+ 
