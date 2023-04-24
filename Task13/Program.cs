29b01b1c3ffec002fe85f30cba9f529d9eca9ac4
«Pavel <kskles@yandex.ru>
2023-04-24 11:00:21 +0700

    Задача 13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13. Напишите программу, которая
+// выводит третью цифру щаланного чисоа или сообщает,
+// что третьей цифры нет
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num;
+int index = 0;
+int index1 = 0;
+
+while(num > 0)
+{
+ num = num/10;
+ index +=1;
+}
+
+while (index1 <= index - 4)
+{ 
+   num1 = num1/10; 
+   index1 +=1;
+}
+Console.WriteLine("{0} -Третья цифра числа", num1%10);
